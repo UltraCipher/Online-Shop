9e4f330851adcd7f42647f4e59473d2a7dc3540a
Mahiya Hoseinpour <mahiyahp3@gmail.com>
2024-02-01 11:40:25 +0330

    wip

--- frontend/src/components/frequentlyAsked/contactus.jsx
+++ frontend/src/components/frequentlyAsked/contactus.jsx
@@ -1,32 +1,47 @@
 import React from "react";
-import "./Contactus.css";
+import "./Contactus.scss";
+import { FaInstagram } from "react-icons/fa6";
 
 function ContactUs() {
   return (
-    <div>
-      <div>
-        <ul class="wrapper">
-          <li class="icon facebook">
-            <span class="tooltip">Facebook</span>
-            <span>
-              <i class="fab fa-facebook-f"></i>
-            </span>
-          </li>
-          <li class="icon twitter">
-            <span class="tooltip">Twitter</span>
-            <span>
-              <i class="fab fa-twitter"></i>
-            </span>
-          </li>
-          <li class="icon instagram">
-            <span class="tooltip">Instagram</span>
-            <span>
-              <i class="fab fa-instagram"></i>
-            </span>
-          </li>
-        </ul>
+    <React.Fragment>
+      <div className="contactUsContainer">
+        <h2>Contact us</h2>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisi
+            vitae suscipit tellus mauris a diam maecenas sed. Dolor sit amet
+            consectetur adipiscing elit. Aliquam id diam maecenas ultricies mi.
+            Ipsum suspendisse ultrices gravida dictum fusce. At volutpat diam ut
+            venenatis tellus in metus. Vitae proin sagittis nisl rhoncus mattis
+            rhoncus urna. Purus semper eget duis at.
+          </p>
+        </div>
+        <div>
+          <ul class="wrapper">
+            <li class="icon WhatsApp">
+              <span class="tooltip">WhatsApp</span>
+              <span>
+                <i class="fab fa-WhatsApp-f"></i>
+              </span>
+            </li>
+            <li class="icon Telegram">
+              <span class="tooltip">Telegram</span>
+              <span>
+                <i class="fab fa-Telegram"></i>
+              </span>
+            </li>
+            <li class="icon instagram">
+              <span class="tooltip">Instagram</span>
+              <span>
+                <i class="fab fa-instagram"></i>
+              </span>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
 export default ContactUs;
