4aa3eb448b873f39cbed065792003e4180935064
Mahiya Hoseinpour <mahiyahp3@gmail.com>
2024-02-16 11:20:47 +0330

    wip

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./components/form/SignUp";
 import Login from "./components/form/Login";
 import Navbar from "./components/navbar/navbar";
-import HomePage from "./components/homePage/homePage";
+import HomePage from "./components/pages/homePage";
 import Error404 from "./components/error-404/error";
 import Footer from "./components/footer/footer";
 import FrequentlyAsked from "./components/frequentlyAsked/frequentlyAsked";
